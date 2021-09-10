6b9a44a5d55a8c40cf8579611e978526a9e8ad34
john <john.d.leonetti@gmail.com>
2021-09-10 13:41:55 -0700

    push data

--- app/controllers/encodertest_controller.rb
+++ app/controllers/encodertest_controller.rb
@@ -1,13 +1,14 @@
-require "base64"
-class EncoderTestController < ApplicationController
+# was test sit to get pics to return
+# require "base64"
+# class EncoderTestController < ApplicationController
 
-    def index   
+#     def index   
 
-    base64_image =
-    File.open("app/image_upload/IMG_9215.jpg", "rb") do |file|
-        # why strict_encode64
-        # https://ruby-doc.org/stdlib-2.3.1/libdoc/base64/rdoc/Base64.html#method-i-strict_encode64
-        Base64.strict_encode64(file.read)
-        end
-    end 
-end
+#     base64_image =
+#     File.open("app/image_upload/IMG_9215.jpg", "rb") do |file|
+#         # why strict_encode64
+#         # https://ruby-doc.org/stdlib-2.3.1/libdoc/base64/rdoc/Base64.html#method-i-strict_encode64
+#         Base64.strict_encode64(file.read)
+#         end
+#     end 
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,10 @@ Property.destroy_all
 
 
 Property.create({
-    property_name: ,
-    description: ,
-    images:
+    property_name: "541 S Paseo del Cobre",
+    description: "This 2 bedroom, 2 full bath townhouse is located in an area called Townhouse IV, in Green Valley, 
+    Arizona. Just off Continental and La Canada. Parking is available in the carport area for 2 cars undercover and 2 more in the driveway, 
+    you enter the home through a gated front courtyard and the somewhat retro front door area, the door has a Smartlock, 
+    I will send you the code 2 weeks prior to your lease starting. The entry has hooks for coats and bags, a convenient table for keys.",
+    images:""
 })
